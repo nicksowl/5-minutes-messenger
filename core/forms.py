88c5f8de53f35fc66e650519d249249dc98216da
Nikolai Baida <nikolai.baida@gmail.com>
2022-10-22 20:02:34 +0200

    First version

---
+++ core/forms.py
@@ -0,0 +1,11 @@
+import django
+
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
